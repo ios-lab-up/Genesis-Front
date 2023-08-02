b5e9d0b20ce28300f566fb5b0a4f030caf7d2a9e
luisced <luis.cedillo.maldonado@gmail.com>
2023-08-01 22:14:15 -0600

    added resend verification code enpoint

--- Genesis/Network/NetworkManager.swift
+++ Genesis/Network/NetworkManager.swift
@@ -14,7 +14,7 @@ class NetworkManager {
     private init() {}
 
     func signUp(name: String, username: String, email: String, password: String, birthDate: String, profileId: Int, completion: @escaping (Result<User, Error>) -> Void) {
-        guard let url = URL(string: "https://a210-189-147-92-102.ngrok-free.app/sign_up") else {
+        guard let url = URL(string: "https://b22b-189-147-118-9.ngrok-free.app/sign_up") else {
             completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
         }
@@ -43,7 +43,7 @@ class NetworkManager {
     }
     
     func verifyIdentity(code: String, completion: @escaping (Result<User, Error>) -> Void) {
-        let url = URL(string: "https://a210-189-147-92-102.ngrok-free.app/sign_up/verify_identity")!
+        let url = URL(string: "https://b22b-189-147-118-9.ngrok-free.app/sign_up/verify_identity")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -70,6 +70,28 @@ class NetworkManager {
         
         task.resume()
     }
+    
+    func resendVerificationCode(completion: @escaping (Result<Bool, Error>) -> Void) {
+        guard let url = URL(string: "https://b22b-189-147-118-9.ngrok-free.app/sign_up/resend_verification_code") else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue(self.jwtToken ?? "", forHTTPHeaderField: "x-access-token" )
+        
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(true))
+            }
+        }
+        task.resume()
+    }
+
 }
 
 struct User: Codable {
--- Genesis/Views/VerifyIdentityView.swift
+++ Genesis/Views/VerifyIdentityView.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct VerifyIdentityView: View {
-    @State private var verificationCode = ""
-    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State private var verificationCode = Array(repeating: "", count: 6)
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Verification")) {
-                    TextField("Verification Code", text: $verificationCode)
+                    HStack {
+                        ForEach(0..<6) { index in
+                            TextField("", text: Binding(
+                                get: { self.verificationCode[index] },
+                                set: { newValue in
+                                    if newValue.count <= 1 && newValue.last?.isNumber == true {
+                                        self.verificationCode[index] = newValue
+                                    }
+                                }
+                            ))
+                            .multilineTextAlignment(.center)
+                            .frame(width: 30, height: 30)
+                            .border(Color.gray, width: 1)
+                            .keyboardType(.numberPad)
+                        }
+                    }
                 }
                 
                 Section {
                     Button(action: verifyIdentity) {
                         Text("Verify Identity")
+                    }
+                    
+                    Button(action: resendCode) {
+                        Text("Resend Code")
                     }
                 }
             }
@@ -29,13 +48,25 @@ struct VerifyIdentityView: View {
     }
     
     func verifyIdentity() {
-        NetworkManager.shared.verifyIdentity(code: verificationCode) { result in
+        let code = verificationCode.joined()
+        NetworkManager.shared.verifyIdentity(code: code) { result in
             switch result {
             case .success(let user):
                 print("Verified user: \(user)")
                 self.presentationMode.wrappedValue.dismiss()
             case .failure(let error):
                 print("Failed to verify user: \(error)")
+            }
+        }
+    }
+    
+    func resendCode() {
+        NetworkManager.shared.resendVerificationCode { result in
+            switch result {
+            case .success:
+                print("Verification code resent successfully.")
+            case .failure(let error):
+                print("Failed to resend verification code: \(error)")
             }
         }
     }
