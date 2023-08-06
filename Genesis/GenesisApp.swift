3ccd38e89b9896e5a97cc8ac07fee9be6cf97311
Iñaki Sigüenza <isiguenzan@gmail.com>
2023-08-05 21:09:29 -0600

    Usage Descriptions and main app bug

--- Genesis/GenesisApp.swift
+++ Genesis/GenesisApp.swift
@@ -10,9 +10,23 @@ import SwiftUI
 
 @main
 struct GenesisApp: App {
+    init() {
+        UINavigationBar.applyCustomAppearance()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CameraView()
         }
+    }
+}
+
+fileprivate extension UINavigationBar {
+    
+    static func applyCustomAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
