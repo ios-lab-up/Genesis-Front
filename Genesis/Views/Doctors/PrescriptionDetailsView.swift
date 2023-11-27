d543756ee7a533ef64a82eecd9b24a0f85e62e7b
luisced <luis.cedillo.maldonado@gmail.com>
2023-11-26 20:50:59 -0600

    merge from detalles

---
+++ Genesis/Views/Doctors/PrescriptionDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  PrescriptionDetailsView.swift
+//  DermAware
+//
+//  Created by Mauricio Pérez on 26/11/23.
+//
+
+import SwiftUI
+
+struct PrescriptionDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrescriptionDetailsView()
+}
